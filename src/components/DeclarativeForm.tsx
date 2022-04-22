db0db2b57ad55fc33bf0a970da6e707a5a6c8999
David Johnston <david@blacksheepcode.com>
2022-04-22 17:37:31 +1000

    Update DeclarativeForm.tsx

--- src/components/DeclarativeForm.tsx
+++ src/components/DeclarativeForm.tsx
@@ -1,10 +1,4 @@
-/*
- * COPYRIGHT NOTICE
- * All source code contained within the Cydarm cybersecurity software provided by Cydarm
- * Technologies Pty Ltd ABN 17 622 236 113 (Company) is the copyright of the Company and
- * protected by copyright laws. Redistribution or reproduction of this material is strictly prohibited
- * without prior written permission of the Company. All rights reserved.
- */
+
 import React from 'react';
 
 export type DeclarativeFormProps<T extends Record<string, unknown>> = {
